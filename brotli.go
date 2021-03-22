ded103d75c1c4a48ead118394161f2ecc6bc82b5
Steffen Brüheim <ueffel@gmail.com>
2021-03-22 17:23:52 +0100

    try cgo binding for brotli

--- brotli.go
+++ brotli.go
@@ -2,12 +2,14 @@ package caddybrotli
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"strconv"
 
-	"github.com/andybalholm/brotli"
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp/encode"
+	"github.com/itchio/go-brotli/enc"
 )
 
 func init() {
@@ -51,13 +53,18 @@ func (b *Brotli) Provision(ctx caddy.Context) error {
 	return nil
 }
 
+const (
+	Bestspeed       = 0
+	Bestcompression = 11
+)
+
 // Validate validates b's configuration.
 func (b Brotli) Validate() error {
-	if b.Level < brotli.BestSpeed {
-		return fmt.Errorf("quality too low; must be >= %d", brotli.BestSpeed)
+	if b.Level < Bestspeed {
+		return fmt.Errorf("quality too low; must be >= %d", Bestspeed)
 	}
-	if b.Level > brotli.BestCompression {
-		return fmt.Errorf("quality too high; must be <= %d", brotli.BestCompression)
+	if b.Level > Bestcompression {
+		return fmt.Errorf("quality too high; must be <= %d", Bestcompression)
 	}
 	return nil
 }
@@ -66,10 +73,34 @@ func (b Brotli) Validate() error {
 // used in the Accept-Encoding request headers.
 func (Brotli) AcceptEncoding() string { return "br" }
 
+type brotliEncoder struct {
+	*enc.BrotliWriter
+	*enc.BrotliWriterOptions
+}
+
+func (b *brotliEncoder) Reset(writer io.Writer) {
+	newWriter := enc.NewBrotliWriter(writer, b.BrotliWriterOptions)
+	b.BrotliWriter = newWriter
+}
+
+func (b *brotliEncoder) Close() error {
+	return b.BrotliWriter.Close()
+}
+
+func (b *brotliEncoder) Write(data []byte) (int, error) {
+	return b.BrotliWriter.Write(data)
+}
+
 // NewEncoder returns a new brotli writer.
 func (b Brotli) NewEncoder() encode.Encoder {
-	writer := brotli.NewWriterLevel(nil, b.Level)
-	return writer
+	options := enc.BrotliWriterOptions{Quality: b.Level}
+	writer := enc.NewBrotliWriter(ioutil.Discard, &options)
+	enc := brotliEncoder{
+		BrotliWriter:        writer,
+		BrotliWriterOptions: &options,
+	}
+
+	return &enc
 }
 
 var defaultBrotliLevel = 4
@@ -80,4 +111,5 @@ var (
 	_ caddy.Provisioner     = (*Brotli)(nil)
 	_ caddy.Validator       = (*Brotli)(nil)
 	_ caddyfile.Unmarshaler = (*Brotli)(nil)
+	_ encode.Encoder        = (*brotliEncoder)(nil)
 )
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ueffel/caddy-brotli
 go 1.14
 
 require (
-	github.com/andybalholm/brotli v1.0.1
 	github.com/caddyserver/caddy/v2 v2.0.0
+	github.com/itchio/go-brotli v0.0.0-20190702114328-3f28d645a45c
 )
